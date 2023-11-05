c6c19bbe8fc53ecb300537aa3e49408a15e2dc5b
Kelrhas <flbonnin@gmail.com>
2023-11-05 17:23:35 +0100

    2021.7

--- 2021/Day7.cpp
+++ 2021/Day7.cpp
@@ -7,7 +7,44 @@ namespace Day7
 		cout << "--- Day 7: The Treachery of Whales ---" << endl;
 		std::ifstream is("Data7.txt");
 
-		cout << "Part1: " << endl;
-		cout << "Part2: " << endl;
+		string sLine;
+		is >> sLine;
+
+		/* example
+		sLine = "16,1,2,0,4,2,7,1,2,14";
+		/**/
+		vector<int> vecNumbers;
+		for (const auto& sub : sLine | views::split(','))
+		{
+			string s(sub.begin(), sub.end());
+			vecNumbers.emplace_back(stoi(s));
+		}
+
+		sort(vecNumbers.begin(), vecNumbers.end());
+		S64 median = vecNumbers[vecNumbers.size() / 2];
+		S64 fuelSimple = 0;
+		for (int n : vecNumbers)
+		{
+			int dist = abs(n - median);
+			fuelSimple += dist;
+		}
+
+		cout << "Part1: " << fuelSimple << endl;
+
+		U64 uBestFuel = -1;
+		for (int h = vecNumbers.front(); h < vecNumbers.back(); ++h)
+		{
+			U64 fuelExpo = 0;
+			for (int n : vecNumbers)
+			{
+				int dist = abs(n - h);
+				fuelExpo += dist * (dist + 1) / 2;
+				if (fuelExpo >= uBestFuel)
+					break;
+			}
+
+			uBestFuel = min(uBestFuel, fuelExpo);
+		}
+		cout << "Part2: " << uBestFuel << endl;
 	}
 } // namespace Day7
